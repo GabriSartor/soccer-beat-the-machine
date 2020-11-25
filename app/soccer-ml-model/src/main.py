09c6e2486b9b25da3ea169c7b0220ecc5482482a
Gabriele Sartor <gabrielesartor4@gmail.com>
2020-11-26 00:06:04 +0100

    ML implementation
    First draf of the ml to predict outcomes.
    Loss rate: 50%, basically it tries to randomly guess the winner

--- app/soccer-ml-model/src/main.py
+++ app/soccer-ml-model/src/main.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import torch as torch
+import matplotlib.pyplot as plt
+import numpy as np
 
-df = pd.read_csv("set.csv")
+df = pd.read_csv("../data/dataset/home_training_set.csv")
 #Che merda
-extract = [1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,19,20,21,23,24] 
+extract = [1,2,3,4,5,6,7,8,9,10,23,24] 
 df = df.iloc[:, extract]
 
 for x in range(len(df.index)):
-    df.iloc[x, len(df.columns)-2] += 1
+    df.iloc[x, len(df.columns)-2]
     df.iloc[x, :-2] /= df.iloc[x, -2]
     
 
@@ -29,8 +31,8 @@ def convert_output_win(source):
 
 
 df = df.sample(frac=1)
-training = df.iloc[:-50]
-test = df.iloc[-50:]
+training = df.iloc[:-100]
+test = df.iloc[-100:]
 
 test_input  = test.iloc[:, :-1]
 test_output = convert_output_win(test.iloc[:, -1:])
@@ -61,13 +63,13 @@ test_input = torch.FloatTensor(test_input.values)
 test_output = torch.FloatTensor(test_output.values)
 
 input_size = training_input.size()[1] # number of features selected
-hidden_size = 30 # number of nodes/neurons in the hidden layer
+hidden_size = 50 # number of nodes/neurons in the hidden layer
 model = Net(input_size, hidden_size) # create the model
 criterion = torch.nn.BCELoss() # works for binary classification
 # without momentum parameter
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.9) 
+#optimizer = torch.optim.SGD(model.parameters(), lr = 0.9) 
 #with momentum parameter
-#optimizer = torch.optim.SGD(model.parameters(), lr = 0.9, momentum=0.2)
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.9, momentum=0.2)
 
 model.eval()
 y_pred = model(test_input)
@@ -75,7 +77,7 @@ before_train = criterion(y_pred.squeeze(), test_output)
 print('Test loss before training' , before_train.item())
 
 model.train()
-epochs = 5000
+epochs = 30000
 errors = []
 for epoch in range(epochs):
     optimizer.zero_grad()
@@ -93,3 +95,20 @@ model.eval()
 y_pred = model(test_input)
 after_train = criterion(y_pred.squeeze(), test_output)
 print('Test loss after Training' , after_train.item())
+
+def plotcharts(errors):
+    errors = np.array(errors)
+    plt.figure(figsize=(12, 5))
+    graf02 = plt.subplot(1, 2, 1) # nrows, ncols, index
+    graf02.set_title('Errors')
+    plt.plot(errors, '-')
+    plt.xlabel('Epochs')
+    graf03 = plt.subplot(1, 2, 2)
+    graf03.set_title('Tests')
+    a = plt.plot(test_output.numpy(), 'yo', label='Real')
+    plt.setp(a, markersize=10)
+    a = plt.plot(y_pred.detach().numpy(), 'b+', label='Predicted')
+    plt.setp(a, markersize=10)
+    plt.legend(loc=7)
+    plt.show()
+plotcharts(errors)
